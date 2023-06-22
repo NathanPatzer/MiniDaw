4dd19acaa87322286b201c0f82e56400aee0268c
Nathan Patzer <patze011@umn.edu>
2023-06-21 23:16:39 -0500

    Not needed right now

--- src/main.rs
+++ src/main.rs
@@ -31,25 +31,6 @@ fn main() {
     let midi_parser: MidiParser::MidiParser = MidiParser::MidiParser::new(args.midi, args.BPM, args.wave);
     let final_samples: Vec<f32> = midi_parser.parse(args.transpose);
     let mut w: Writer = Writer::new(args.name);
-
-    let sin: Sin = Sin::new(2000.0);
-    let test_samples: Vec<f32> = sin.gen(500.0, 10.0);
-
-    let mut currBuf: Vec<Complex<f32>> = Vec::new();
-    for i in 0..test_samples.len()
-    {
-        if i % 4096 == 0 && i != 0
-        {
-            let mut buff = currBuf.clone();
-            DSP::DSP::FFT(&mut buff);
-            println!("{}",DSP::DSP::find_freq(buff, 44100));
-            currBuf.clear();
-            currBuf.push(Complex::new(test_samples[i], 0.0));
-        }
-        else {
-            currBuf.push(Complex::new(test_samples[i], 0.0));
-        }
-    }
     w.append(final_samples.clone());
     w.write(); 
 }
